1211490a255e0a76a3e865f8a9d2aa3fa5966720
ivanarifin <ivan.rarifin@gmail.com>
2022-11-30 21:05:10 +0700

    push ulang dengan multer

--- server/apollo/app.js
+++ server/apollo/app.js
@@ -1,14 +1,14 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import userScheme from "./schemes/userScheme"
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const userScheme = require("./schemes/userScheme");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  startStandaloneServer(server, {
-    listen: { port: process.env.PORT || 4000 },
-  }).then(({ url }) => {
-    console.log(`🚀  Server ready at: ${url}`);
-  });
+  typeDefs: [userScheme.typeDefs],
+  resolvers: [userScheme.resolvers]
+});
+
+startStandaloneServer(server, {
+  listen: { port: process.env.PORT || 4000 },
+}).then(({ url }) => {
+  console.log(`🚀  Server ready at: ${url}`);
+});
--- server/apollo/schemes/userScheme.js
+++ server/apollo/schemes/userScheme.js
@@ -1,86 +1,133 @@
 const axios = require("axios");
-// const userUrl = "https://p3-challenge-2-ivan-users-production.up.railway.app/users/";
+const userUrl = "http://localhost:3000/users/";
+const { GraphQLScalarType } = require("graphql");
+const { Kind } = require("graphql/language");
 
-// const typeDefs = `#graphql
+const typeDefs = `#graphql
 
-// type User{
-//     username: String
-//     email: String
-//     role: String
-//     password: String
-//     phoneNumber: String
-//     address: String
-// }
+scalar Date
 
-// input userInput{
-//   username: String
-//   email: String
-//   role: String
-//   password: String
-//   phoneNumber: String
-//   address: String
-// }
+input loginInput{
+  email: String
+  password: String
+}
+type login{
+    access_token: String
+    email: String
+    id: ID
+    username: String
+}
 
-// type Users{
-//   _id: String
-//   username: String
-//   email: String
-//   role: String
-//   password: String
-//   phoneNumber: String
-//   address: String
-// }
+type msg{
+  msg: String
+}
 
-// type Query {
-//   getUsers: [Users]
-//   getUserById(id: ID) : User
-// }
+input registerInput{
+  username: String
+  email: String
+  password: String
+  dob: Date
+  domisili : String
+  gender: String
+}
 
-// type Mutation {
-//     addUser(newUser: userInput) : User
-//     delUser(_id: String) : User
-//   }
-// `;
+type User{
+    username: String
+    email: String
+    role: String
+    password: String
+    phoneNumber: String
+    address: String
+}
 
-// const resolvers = {
-//   Query: {
-//     getUsers: async () => {
-//       try {
-//         let { data } = await axios.get(userUrl);
-//         return data;
-//       } catch (error) {
-//         throw new GraphQLError("Internal Server Error");
-//       }
-//     },
-//     getUserById: async (_, args) => {
-//       try {
-//         const { id } = args;
-//         let { data } = await axios.get(userUrl + id);
-//         return data;
-//       } catch (error) {
-//         throw new GraphQLError("Internal Server Error");
-//       }
-//     },
-//   },
-//   Mutation: {
-//     addUser: async (_, args) => {
-//       try {
-//         const { data } = await axios.post(userUrl, args.newUser);
-//         return { msg: "User has been added" };
-//       } catch (error) {
-//         throw new GraphQLError("Internal Server Error");
-//       }
-//     },
-//     delUser: async (_, args) => {
-//       try {
-//         const { _id } = args;
-//         const { data } = await axios.delete(userUrl + _id);
-//         return data;
-//       } catch (error) {
-//         throw new GraphQLError("Internal Server Error");
-//       }
-//     },
-//   },
-// };
+type Users{
+    id: ID
+    username: String
+    email: String
+    role: String
+    password: String
+    phoneNumber: String
+    address: String
+  }
+
+type Query {
+    getUsers: [Users]
+    getUserById(id: ID) : User
+  }
+
+type Mutation {
+    loginUser(login:loginInput) : login
+    registerUser(register:registerInput) : msg
+  }
+`;
+
+const resolverMap = {
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date custom scalar type",
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10); // ast value is always in string format
+      }
+      return null;
+    },
+  }),
+};
+
+const resolvers = {
+  Date: resolverMap,
+  Query: {
+    getUsers: async () => {
+      try {
+        let { data } = await axios.get(userUrl);
+        return data;
+      } catch (error) {
+        throw new GraphQLError("Internal Server Error");
+      }
+    },
+    getUserById: async (_, args) => {
+      try {
+        const { id } = args;
+        let { data } = await axios.get(userUrl + id);
+        return data;
+      } catch (error) {
+        throw new GraphQLError("Internal Server Error");
+      }
+    },
+  },
+  Mutation: {
+    registerUser: async (_, args) => {
+      try {
+        const { data } = await axios.post(userUrl + "register", args.register);
+        return data;
+      } catch (error) {
+        throw new GraphQLError("Internal Server Error");
+      }
+    },
+    // delUser: async (_, args) => {
+    //   try {
+    //     const { _id } = args;
+    //     const { data } = await axios.delete(userUrl + _id);
+    //     return data;
+    //   } catch (error) {
+    //     throw new GraphQLError("Internal Server Error");
+    //   }
+    // },
+    loginUser: async (_, args) => {
+      try {
+        const { data } = await axios.post(userUrl + "login", args.login);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 
 module.exports = { resolvers, typeDefs };
