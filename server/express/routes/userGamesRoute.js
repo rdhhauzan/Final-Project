ac5589f851138d4a86ecc2df7d745eb7bea43488
valtjoker <adjiugrohoseno@gmail.com>
2022-12-02 13:15:38 +0700

    seeded users

--- server/express/routes/userGamesRoute.js
+++ server/express/routes/userGamesRoute.js
@@ -4,5 +4,7 @@ const UserGameController = require("../controllers/UserGameController");
 
 router.get("/", UserGameController.getAllUserGames);
 router.get("/:id", UserGameController.getUserGame);
+router.post("/", UserGameController.createUserGame);
+router.put("/:id", UserGameController.editUserGame);
 
 module.exports = router;
