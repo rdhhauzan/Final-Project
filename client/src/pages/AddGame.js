e11cb5a82f96af75847cffc9acfc80ee2cbe86f6
valtjoker <adjiugrohoseno@gmail.com>
2022-12-05 21:41:55 +0700

    on progress add user game (-axios)

--- client/src/pages/AddGame.js
+++ client/src/pages/AddGame.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchGames } from "../store/actions/action";
+import { addUserGame, fetchGames } from "../store/actions/action";
 import normalBg from "../assets/gs.png";
 import mlBg from "../assets/gameBg/mlbb.jpg";
 import valoBg from "../assets/gameBg/valo.jpg";
@@ -29,6 +29,10 @@ export default function AddGame() {
     const { name, value } = event.target;
     setForm({ ...form, [name]: value });
   };
+  const onSubmitHandler = (id) => {
+    console.log(form, id);
+    // dispatch(addUserGame(form));
+  };
   return (
     <div
       className="overflow-hidden h-screen flex flex-col justify-center bg-cover transition-all"
@@ -39,10 +43,20 @@ export default function AddGame() {
           <p className="text-2xl font-poppins font-medium self-center">
             Please input your game info
           </p>
-          <form className="container m-5 flex flex-col gap-5">
+          <form
+            className="container m-5 flex flex-col gap-5"
+            onSubmit={(event) => {
+              event.preventDefault();
+              onSubmitHandler(game.id);
+            }}
+          >
             <div className="flex flex-col">
               <label className="text-lg font-poppins">Role</label>
-              <select className="h-8 w-96">
+              <select
+                className="h-8 w-96"
+                onChange={onChangeHandler}
+                name="role"
+              >
                 <option selected disabled>
                   --Choose Role--
                 </option>
@@ -53,7 +67,11 @@ export default function AddGame() {
             </div>
             <div className="flex flex-col">
               <label className="text-lg font-poppins">Rank</label>
-              <select className="h-8 w-96">
+              <select
+                className="h-8 w-96"
+                onChange={onChangeHandler}
+                name="rank"
+              >
                 <option selected disabled>
                   --Choose Rank--
                 </option>
@@ -66,7 +84,11 @@ export default function AddGame() {
               <label className="text-lg font-poppins">
                 What do you play for?
               </label>
-              <select className="h-8 w-96">
+              <select
+                className="h-8 w-96"
+                onChange={onChangeHandler}
+                name="matchType"
+              >
                 <option selected disabled>
                   --Choose Match Type--
                 </option>
@@ -81,7 +103,11 @@ export default function AddGame() {
               <p className="text-sm font-thin text-dimWhite mb-1">
                 eg. "i am an agressive player"
               </p>
-              <input className="h-8 w-96" />
+              <input
+                className="h-8 w-96"
+                onChange={onChangeHandler}
+                name="aboutMe"
+              />
             </div>
             <div className="flex flex-row justify-between w-3/4 ml-7">
               <button
