3a3e4f77b38c2fbf5dd6c6d64ad779657b9e9039
valtjoker <adjiugrohoseno@gmail.com>
2022-12-08 14:24:24 +0700

    fixed profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 import ripple from "../assets/ripple.svg";
 
 import {
-	editUser,
-	editUserGame,
-	fetchGames,
-	fetchUserById,
-	followFriend,
+  editUser,
+  editUserGame,
+  fetchGames,
+  fetchUserById,
+  followFriend,
 } from "../store/actions/action";
 export default function Profile() {
   const dispatch = useDispatch();
@@ -23,43 +23,43 @@ export default function Profile() {
     image: "",
   });
 
-	const [editGame, setEditGame] = useState({
-		rank: "",
-		role: "",
-		matchType: "",
-		aboutMe: "",
-	});
+  const [editGame, setEditGame] = useState({
+    rank: "",
+    role: "",
+    matchType: "",
+    aboutMe: "",
+  });
 
-	useEffect(() => {
-		dispatch(fetchUserById(id));
-		dispatch(fetchGames);
-		// eslint-disable-next-line
-	}, [userDetail]);
+  useEffect(() => {
+    dispatch(fetchUserById(id));
+    dispatch(fetchGames);
+    // eslint-disable-next-line
+  }, []);
 
-	const onSubmitHandler = (event) => {
-		event.preventDefault();
-		let formData = new FormData();
-		formData.append("username", form.username);
-		formData.append("password", form.password);
-		formData.append("domisili", form.domisili);
-		if (form.image) formData.append("image", form.image);
-		dispatch(editUser(formData, userDetail?.user?.id));
-		setClicked(false);
-	};
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("username", form.username);
+    formData.append("password", form.password);
+    formData.append("domisili", form.domisili);
+    if (form.image) formData.append("image", form.image);
+    dispatch(editUser(formData, userDetail?.user?.id));
+    setClicked(false);
+  };
 
-	const onChangeHandler = (event) => {
-		let { name, value } = event.target;
-		if (name === "image") {
-			let image = event.target.files[0];
-			value = image;
-		}
-		setForm({ ...form, [name]: value });
-	};
+  const onChangeHandler = (event) => {
+    let { name, value } = event.target;
+    if (name === "image") {
+      let image = event.target.files[0];
+      value = image;
+    }
+    setForm({ ...form, [name]: value });
+  };
 
-	const onChangeModal = (event) => {
-		let { name, value } = event.target;
-		setEditGame({ ...editGame, [name]: value });
-	};
+  const onChangeModal = (event) => {
+    let { name, value } = event.target;
+    setEditGame({ ...editGame, [name]: value });
+  };
 
   const onSubmitModal = (gameId) => {
     dispatch(editUserGame(editGame, gameId));
@@ -234,196 +234,206 @@ export default function Profile() {
           </div>
         )}
 
-				<div className="flex flex-col w-full basis-1/2 gap-3 xl:mt-0 md:mt-4">
-					{userDetail?.user?.Posts.map((post) => {
-						return (
-							<div className="card w-full bg-primary rounded shadow-xl shadow-black flex justify-center">
-								<div className="card-body text-start">
-									<h2 className="card-title">@{userDetail?.user?.username}</h2>
-									<p>{post.content}</p>
-									{post.imgUrl ? (
-										<figure className="pt-5 w-64 h-auto">
-											<img
-												src={post.imgUrl}
-												alt="Shoes"
-												className="rounded-xl w-full"
-											/>
-										</figure>
-									) : null}
-								</div>
-							</div>
-						);
-					})}
-				</div>
-				<div className="flex flex-col xl:mt-0 2xs:mt-4 w-full basis-1/4 gap-5">
-					{userDetail?.user?.UserGames.map((game) => {
-						return (
-							<div
-								className="card xl:w-96 md:w-full h-auto flex justify-center"
-								key={game.id}>
-								<label
-									htmlFor={game.id}
-									className="btn bg-primary hover:bg-primary hover:border-primary border-primary hover:scale-105 w-fit h-fit hover:bg-none">
-									<img src={game.Game.imgUrl} />
-								</label>
-								<input type="checkbox" id={game.id} className="modal-toggle" />
-								<label
-									className="bg-black flex flex-col items-center bg-opacity-90 h-auto modal"
-									htmlFor={game.id}>
-									<label
-										className="modal-box relative flex flex-col gap-5"
-										htmlFor="">
-										<div className="flex flex-row justify-center border-b border-gray-600 pb-2">
-											<p className="text-slate-300 text-lg font-semibold">
-												{userDetail.user.username}'s {game.Game.name} profile
-											</p>
-										</div>
-										{modalClick ? (
-											<form className="flex flex-col gap-3">
-												<div className="flex flex-row w-full justify-around">
-													<div>
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Rank
-														</p>
-														<select
-															className="text-sm rounded-sm  bg-[#20252e]"
-															onChange={onChangeModal}
-															name="rank"
-															value={editGame.rank}>
-															{game.Game.rankList.map((rank, index) => {
-																return <option value={index}>{rank}</option>;
-															})}
-														</select>
-													</div>
-													<div>
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Type
-														</p>
-														<select
-															className="text-sm rounded-sm  bg-[#20252e]"
-															onChange={onChangeModal}
-															name="matchType"
-															value={editGame.matchType}>
-															<option value="Competitive">Competitive</option>
-															<option value="Casual">Casual</option>
-														</select>
-													</div>
+        <div className="flex flex-col w-full basis-1/2 gap-3 xl:mt-0 md:mt-4">
+          {userDetail?.user?.Posts.map((post) => {
+            return (
+              <div className="card w-full bg-primary rounded shadow-xl shadow-black flex justify-center">
+                <div className="card-body text-start">
+                  <h2 className="card-title">@{userDetail?.user?.username}</h2>
+                  <p>{post.content}</p>
+                  {post.imgUrl ? (
+                    <figure className="pt-5 w-64 h-auto">
+                      <img
+                        src={post.imgUrl}
+                        alt="Shoes"
+                        className="rounded-xl w-full"
+                      />
+                    </figure>
+                  ) : null}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex flex-col xl:mt-0 2xs:mt-4 w-full basis-1/4 gap-5">
+          {userDetail?.user?.UserGames.map((game) => {
+            return (
+              <div
+                className="card xl:w-96 md:w-full h-auto flex justify-center"
+                key={game.id}
+              >
+                <label
+                  htmlFor={game.id}
+                  className="btn bg-primary hover:bg-primary hover:border-primary border-primary hover:scale-105 w-fit h-fit hover:bg-none"
+                >
+                  <img src={game.Game.imgUrl} />
+                </label>
+                <input type="checkbox" id={game.id} className="modal-toggle" />
+                <label
+                  className="bg-black flex flex-col items-center bg-opacity-90 h-auto modal"
+                  htmlFor={game.id}
+                >
+                  <label
+                    className="modal-box relative flex flex-col gap-5"
+                    htmlFor=""
+                  >
+                    <div className="flex flex-row justify-center border-b border-gray-600 pb-2">
+                      <p className="text-slate-300 text-lg font-semibold">
+                        {userDetail.user.username}'s {game.Game.name} profile
+                      </p>
+                    </div>
+                    {modalClick ? (
+                      <form className="flex flex-col gap-3">
+                        <div className="flex flex-row w-full justify-around">
+                          <div>
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Rank
+                            </p>
+                            <select
+                              className="text-sm rounded-sm  bg-[#20252e]"
+                              onChange={onChangeModal}
+                              name="rank"
+                              value={editGame.rank}
+                            >
+                              {game.Game.rankList.map((rank, index) => {
+                                return <option value={index}>{rank}</option>;
+                              })}
+                            </select>
+                          </div>
+                          <div>
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Type
+                            </p>
+                            <select
+                              className="text-sm rounded-sm  bg-[#20252e]"
+                              onChange={onChangeModal}
+                              name="matchType"
+                              value={editGame.matchType}
+                            >
+                              <option value="Competitive">Competitive</option>
+                              <option value="Casual">Casual</option>
+                            </select>
+                          </div>
 
-													<div className="">
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Role
-														</p>
-														<select
-															className="text-sm rounded-sm  bg-[#20252e]"
-															onChange={onChangeModal}
-															name="role"
-															value={editGame.role}>
-															{game.Game.roleList.map((role) => {
-																return <option value={role}>{role}</option>;
-															})}
-														</select>
-													</div>
-												</div>
+                          <div className="">
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Role
+                            </p>
+                            <select
+                              className="text-sm rounded-sm  bg-[#20252e]"
+                              onChange={onChangeModal}
+                              name="role"
+                              value={editGame.role}
+                            >
+                              {game.Game.roleList.map((role) => {
+                                return <option value={role}>{role}</option>;
+                              })}
+                            </select>
+                          </div>
+                        </div>
 
-												<div className="">
-													<p className="text-slate-300 text-sm mb-2 font-semibold">
-														About
-													</p>
-													<input
-														className="text-slate-300 text-sm bg-[#20252e] p-2 rounded-md w-full"
-														value={editGame.aboutMe}
-														onChange={onChangeModal}
-														name="aboutMe"
-													/>
-												</div>
-												<div className="flex flex-row justify-evenly">
-													<button
-														className="btn w-fit btn-sm hover:bg-red-500 hover:text-white border-none"
-														onClick={(e) => {
-															e.preventDefault();
-															setModalClick(false);
-															setEditGame({
-																rank: "",
-																role: "",
-																matchType: "",
-																aboutMe: "",
-															});
-														}}>
-														Cancel
-													</button>
-													<label
-														onClick={(e) => {
-															onSubmitModal(game.id);
-														}}
-														className="btn w-fit btn-sm hover:bg-info hover:text-white border-none"
-														type="submit">
-														Submit
-													</label>
-												</div>
-											</form>
-										) : (
-											<div className="flex flex-col gap-3">
-												<div className="flex flex-row w-full justify-around">
-													<div>
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Rank
-														</p>
-														<p className="text-slate-300 text-sm text-center">
-															{game.Game.rankList[game.rank]}
-														</p>
-													</div>
-													<div>
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Type
-														</p>
-														<p className="text-slate-300 text-sm text-center">
-															{game.matchType}
-														</p>
-													</div>
+                        <div className="">
+                          <p className="text-slate-300 text-sm mb-2 font-semibold">
+                            About
+                          </p>
+                          <input
+                            className="text-slate-300 text-sm bg-[#20252e] p-2 rounded-md w-full"
+                            value={editGame.aboutMe}
+                            onChange={onChangeModal}
+                            name="aboutMe"
+                          />
+                        </div>
+                        <div className="flex flex-row justify-evenly">
+                          <button
+                            className="btn w-fit btn-sm hover:bg-red-500 hover:text-white border-none"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setModalClick(false);
+                              setEditGame({
+                                rank: "",
+                                role: "",
+                                matchType: "",
+                                aboutMe: "",
+                              });
+                            }}
+                          >
+                            Cancel
+                          </button>
+                          <label
+                            onClick={(e) => {
+                              onSubmitModal(game.id);
+                            }}
+                            className="btn w-fit btn-sm hover:bg-info hover:text-white border-none"
+                            type="submit"
+                          >
+                            Submit
+                          </label>
+                        </div>
+                      </form>
+                    ) : (
+                      <div className="flex flex-col gap-3">
+                        <div className="flex flex-row w-full justify-around">
+                          <div>
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Rank
+                            </p>
+                            <p className="text-slate-300 text-sm text-center">
+                              {game.Game.rankList[game.rank]}
+                            </p>
+                          </div>
+                          <div>
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Type
+                            </p>
+                            <p className="text-slate-300 text-sm text-center">
+                              {game.matchType}
+                            </p>
+                          </div>
 
-													<div>
-														<p className="text-slate-300 text-sm text-center font-semibold">
-															Role
-														</p>
-														<p className="text-slate-300 text-sm text-center">
-															{game.role}
-														</p>
-													</div>
-												</div>
-												<div className="">
-													<p className="text-slate-300 text-sm mb-2 font-semibold">
-														About
-													</p>
-													<p className="text-slate-300 text-sm bg-[#111419] p-2 rounded-md">
-														{game.aboutMe}
-													</p>
-												</div>
-												{+localStorage.getItem("id") !==
-												userDetail?.user?.id ? null : (
-													<button
-														className="btn w-fit self-end btn-sm hover:bg-tertiary hover:text-white border-none"
-														onClick={(e) => {
-															e.preventDefault();
-															setModalClick(true);
-															setEditGame({
-																rank: game.rank,
-																role: game.role,
-																matchType: game.matchType,
-																aboutMe: game.aboutMe,
-															});
-														}}>
-														Edit
-													</button>
-												)}
-											</div>
-										)}
-									</label>
-								</label>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-	);
+                          <div>
+                            <p className="text-slate-300 text-sm text-center font-semibold">
+                              Role
+                            </p>
+                            <p className="text-slate-300 text-sm text-center">
+                              {game.role}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="">
+                          <p className="text-slate-300 text-sm mb-2 font-semibold">
+                            About
+                          </p>
+                          <p className="text-slate-300 text-sm bg-[#111419] p-2 rounded-md">
+                            {game.aboutMe}
+                          </p>
+                        </div>
+                        {+localStorage.getItem("id") !==
+                        userDetail?.user?.id ? null : (
+                          <button
+                            className="btn w-fit self-end btn-sm hover:bg-tertiary hover:text-white border-none"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setModalClick(true);
+                              setEditGame({
+                                rank: game.rank,
+                                role: game.role,
+                                matchType: game.matchType,
+                                aboutMe: game.aboutMe,
+                              });
+                            }}
+                          >
+                            Edit
+                          </button>
+                        )}
+                      </div>
+                    )}
+                  </label>
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
