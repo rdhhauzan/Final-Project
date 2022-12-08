cee29e8ade5de4654ab47a36003d85cd50171dda
ivanarifin <ivan.rarifin@gmail.com>
2022-12-08 19:45:15 +0700

    fix seeding with promise

--- server/express/seeders/20221202055542-seed-users.js
+++ server/express/seeders/20221202055542-seed-users.js
@@ -17,43 +17,42 @@ module.exports = {
      * }], {});
      */
     let users = await Promise.all(
-      data.map(async (e) => {
+      data.map((e) => {
         let uuid = uuidv4();
-        try {
-          const options = {
-            method: "POST",
-            url: "https://2269480a5983d987.api-us.cometchat.io/v3/users",
-            headers: {
-              apiKey: "dd160c53b176e730b4e702acbc12a2ddfc921eda",
-              "Content-Type": "application/json",
-              Accept: "application/json",
-            },
-            data: {
-              metadata: {
-                "@private": {
-                  email: "user@email.com",
-                  contactNumber: "0123456789",
-                },
+        const options = {
+          method: "POST",
+          url: "https://2269480a5983d987.api-us.cometchat.io/v3/users",
+          headers: {
+            apiKey: "dd160c53b176e730b4e702acbc12a2ddfc921eda",
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          data: {
+            metadata: {
+              "@private": {
+                email: "user@email.com",
+                contactNumber: "0123456789",
               },
-              uid: uuid,
-              name: e.username,
-              avatar: `https://avatars.dicebear.com/api/initials/${e.username}.svg`,
             },
-          };
+            uid: uuid,
+            name: e.username,
+            avatar: `https://avatars.dicebear.com/api/initials/${e.username}.svg`,
+          },
+        };
 
-          await axios.request(options);
-          e.uuid = uuid;
-          e.createdAt = e.updatedAt = new Date();
-          e.dob = new Date(e.dob);
-          e.profPict = `https://avatars.dicebear.com/api/initials/${e.username}.svg`;
-          e.isValid = true;
-          e.isPremium = false;
-          e.isLogin = true;
-          e.password = hashPassword(e.password);
-          return e;
-        } catch (error) {
-          console.log(error);
-        }
+        // await axios.request(options);
+        axios.request(options).then(function (response) {
+          console.log(response);
+        });
+        e.uuid = uuid;
+        e.createdAt = e.updatedAt = new Date();
+        e.dob = new Date(e.dob);
+        e.profPict = `https://avatars.dicebear.com/api/initials/${e.username}.svg`;
+        e.isValid = true;
+        e.isPremium = false;
+        e.isLogin = true;
+        e.password = hashPassword(e.password);
+        return e;
       })
     );
     // const users = data.map((e) => {
@@ -79,11 +78,11 @@ module.exports = {
     //     },
     //   };
 
-    //   axios
-    //     .request(options)
-    //     .then(function (response) {
-    //       console.log(response);
-    //     })
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
     //     .catch(function (error) {});
 
     //   e.uuid = uuid;
